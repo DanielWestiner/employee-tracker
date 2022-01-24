adc5f3cca070c899ec6eda61cc80fd6582292d5e
DanielWestiner <daniel.westiner@gmail.com>
2022-01-23 23:53:23 -0500

    updated schema with primary keys and foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -5,20 +5,29 @@ USE employee_db;
 
 CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHARD(30)
+  name VARCHAR(30)
 );
 
-CREATE TABLE role (
-  id INT PRIMARY KEY,
+CREATE TABLE roles (
+  id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
-  salary DECIMAL(),
-  department_id INT  
+  salary DECIMAL(12,2),
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL 
 );
 
-CREATE TABLE employee (
-  id INT PRIMARY KEY,
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
   role_id INT,
-  manager_id INT
+  FOREIGN KEY (role_id)
+  REFERENCES roles (id)
+  ON DELETE SET NULL,
+  manager_id INT,
+  FOREIGN KEY (manager_id)
+  REFERENCES employees(id)
+  ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,11 @@
+USE employee_db;
 INSERT INTO department (name)
 VALUES ("Sales"),
        ("Engineering"),
        ("Finance"),
        ("Legal");
        
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES ("CEO", 250000, 1),
        ("VP", 175000, 1),
        ("Lead Manager", 95000, 2),
@@ -15,12 +16,12 @@ VALUES ("CEO", 250000, 1),
        ("Account Manager", 83000, 4);
        
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Franklin", "Hart", 1, null),
+VALUES ("Franklin", "Hart", 1, NULL),
        ("Sterling" "Hayden", 2, 1),
-       ("Doralee", "Rhodes" 3, null),
-       ("Violet", "Newstead" 4, 2),
-       ("Judy", "Bernly", 5, null),
+       ("Doralee", "Rhodes", 3, NULL),
+       ("Violet", "Newstead", 4, 2),
+       ("Judy", "Bernly", 5, NULL),
        ("Bob", "Enright", 6, 3),
-       ("Chuck", "Strell", 7, null),
+       ("Chuck", "Strell", 7, NULL),
        ("Reynaldo", "Villalobos", 8, 4);
        
